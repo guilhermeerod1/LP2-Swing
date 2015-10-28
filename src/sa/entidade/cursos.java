a7bdac65ca7c235d2db5cff543fdcbef1c771600
fasm19 <felipe_aumendes16@hotmail.comgit config --global user.name fasm19git commit -m 'Arquivos documentos e cursos adicionados'git config --global user.email felipe_aumendes16@hotmail.com>
2015-10-28 21:48:25 -0200

    Cursos e documentos atualizados
    OK

---
+++ src/sa/entidade/cursos.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sa.entidade;
+
+/**
+ *
+ * @author Felipe
+ */
+public class cursos {
+    
+    private int idCurso;
+    private int idCoordenador;
+    private int idAluno;
+    private String nome;
+    private int tipo;
+    private int cargaHoraria;
+    private int cargaHorariaAC;
+
+    public int getIdCurso() {
+        return idCurso;
+    }
+
+    public void setIdCurso(int idCurso) {
+        this.idCurso = idCurso;
+    }
+
+    public int getIdCoordenador() {
+        return idCoordenador;
+    }
+
+    public void setIdCoordenador(int idCoordenador) {
+        this.idCoordenador = idCoordenador;
+    }
+
+    public int getIdAluno() {
+        return idAluno;
+    }
+
+    public void setIdAluno(int idAluno) {
+        this.idAluno = idAluno;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public int getCargaHorariaAC() {
+        return cargaHorariaAC;
+    }
+
+    public void setCargaHorariaAC(int cargaHorariaAC) {
+        this.cargaHorariaAC = cargaHorariaAC;
+    }
+    
+    
+    
+}
---
+++ src/sa/entidade/documentos.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sa.entidade;
+
+/**
+ *
+ * @author Felipe
+ */
+public class documentos {
+    
+    private int idDocumento;
+    private int idAluno;
+    private char comprovanteEM; 
+    private char comprovanteResidencia;
+    private char copiaAlistamentoMilitar;
+    private char copiaRG;
+    private char copiaCertidaoCasamento;
+    private char historicoEscolar;
+
+    public int getIdDocumento() {
+        return idDocumento;
+    }
+
+    public void setIdDocumento(int idDocumento) {
+        this.idDocumento = idDocumento;
+    }
+
+    public int getIdAluno() {
+        return idAluno;
+    }
+
+    public void setIdAluno(int idAluno) {
+        this.idAluno = idAluno;
+    }
+
+    public char getComprovanteEM() {
+        return comprovanteEM;
+    }
+
+    public void setComprovanteEM(char comprovanteEM) {
+        this.comprovanteEM = comprovanteEM;
+    }
+
+    public char getComprovanteResidencia() {
+        return comprovanteResidencia;
+    }
+
+    public void setComprovanteResidencia(char comprovanteResidencia) {
+        this.comprovanteResidencia = comprovanteResidencia;
+    }
+
+    public char getCopiaAlistamentoMilitar() {
+        return copiaAlistamentoMilitar;
+    }
+
+    public void setCopiaAlistamentoMilitar(char copiaAlistamentoMilitar) {
+        this.copiaAlistamentoMilitar = copiaAlistamentoMilitar;
+    }
+
+    public char getCopiaRG() {
+        return copiaRG;
+    }
+
+    public void setCopiaRG(char copiaRG) {
+        this.copiaRG = copiaRG;
+    }
+
+    public char getCopiaCertidaoCasamento() {
+        return copiaCertidaoCasamento;
+    }
+
+    public void setCopiaCertidaoCasamento(char copiaCertidaoCasamento) {
+        this.copiaCertidaoCasamento = copiaCertidaoCasamento;
+    }
+
+    public char getHistoricoEscolar() {
+        return historicoEscolar;
+    }
+
+    public void setHistoricoEscolar(char historicoEscolar) {
+        this.historicoEscolar = historicoEscolar;
+    }
+    
+    
+}
